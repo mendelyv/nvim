2aadc3fb3d5c268a8484cf8537709ca90e8e65a6
yanghaoran <1064137887@qq.com>
2023-08-06 13:43:03 +0800

    【简要描述】调整NerdFont Icons

--- lua/utils/icons.lua
+++ lua/utils/icons.lua
@@ -3,62 +3,62 @@ local M = {}
 M.diagnostic = {
   Error = "",
   Warn = "",
-  Info = "",
-  Hint = "",
+  Info = "",
+  Hint = "󰍩",
 }
 
 M.diagnostic_thin = {
-  Error = "",
-  Warn = "",
-  Info = "ﬤ",
-  Hint = "",
+  Error = "",
+  Warn = "",
+  Info = "",
+  Hint = "󰍪",
 }
 
 M.tag_level = {
-  Fixme = "ﰡ",
-  Hack = "ﰠ",
-  Warn = "",
-  Note = "ﮉ",
-  Todo = "ﮉ",
-  Perf = "ﮉ",
+  Fixme = "󰁨", -- 表示需要修复的问题
+  Hack = "󰜢", -- 表示在代码中使用了一些技巧或不常见的方法
+  Warn = "󰓹", -- 表示警告，可能是代码中的一些潜在问题
+  Note = "󰓹", -- 表示注释，可能是对代码的一些解释或说明
+  Todo = "󱩺", -- 表示待办事项，需要在之后的工作中处理的任务
+  Perf = "󱤇", -- 表示性能相关的事项，可能是优化的建议或注意事项
 }
 
 M.lsp_kind = {
   -- lsp type
-  String = "",
-  Number = "",
-  Boolean = "◩",
-  Array = "",
-  Object = "",
-  Key = "",
-  Null = "ﳠ",
+  String = "",
+  Number = "󰎠",
+  Boolean = "",
+  Array = "",
+  Object = "󰅩",
+  Key = "",
+  Null = "󰟢",
   -- lsp kind
-  Text = "",
-  Method = "",
-  Function = "",
-  Constructor = "",
+  Text = "",
+  Method = "󰊕",
+  Function = "󰊕",
+  Constructor = "",
   Namespace = "",
-  Field = "ﰠ",
-  Variable = "ﳋ",
-  Class = "",
-  Interface = "",
-  Module = "ﰪ",
+  Field = "",
+  Variable = "",
+  Class = "",
+  Interface = "",
+  Module = "󰜬",
   Property = "",
-  Unit = "塞",
-  Value = "",
+  Unit = "󰕤",
+  Value = "󱀍",
   Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
+  Keyword = "",
+  Snippet = "󱚛",
+  Color = "",
+  File = "",
+  Reference = "",
+  Folder = "󰉖",
   EnumMember = "",
-  Constant = "",
-  Struct = "﬌",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+  Constant = "",
+  Struct = "",
+  Event = "",
+  Operator = "",
+  TypeParameter = "",
 }
 
 -- 获取图标分组
