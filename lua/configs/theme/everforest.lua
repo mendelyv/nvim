5cb404571550e4db3a647dd06a8547aaef49a958
yanghaoran <37864333+mendelyv@users.noreply.github.com>
2024-03-19 11:04:44 +0800

    【简要描述】：调整主题配置
    1.开启性能模式
    2.调整当前行号显示

--- lua/configs/theme/everforest.lua
+++ lua/configs/theme/everforest.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.load()
   vim.g.everforest_background = 'soft'
   vim.g.everforest_disable_italic_comment = 1
+  vim.g.everforest_better_performance = 1
   -- vim.g.everforest_current_word = "bold"
   -- vim.g.everforest_colors_override = {
   --   grey0 = {"#ff0000", "243"},
@@ -10,6 +11,7 @@ function M.load()
   --   grey2 = {"#0000ff", "247"},
   -- }
   vim.cmd [[colorscheme everforest]]
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#B5B5B5", bold = true, italic = true })
 end
 
 return M
