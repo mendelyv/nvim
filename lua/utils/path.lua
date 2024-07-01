e58b2e8715c3c92b2d9bb12652be5d58b4eedf99
mendelyv <1064137887@qq.com>
2024-07-01 18:24:26 +0800

    fix(path):use vim.iter:flatten replace vim.tbl_flatten

--- lua/utils/path.lua
+++ lua/utils/path.lua
@@ -3,7 +3,7 @@ local M = {}
 
 -- 拼接文件路径，使用"/"分隔
 function M.join(...)
-  return table.concat(vim.tbl_flatten({ ... }), "/")
+  return table.concat(vim.iter({ ... }):flatten():totable(), "/")
 end
 
 return M
