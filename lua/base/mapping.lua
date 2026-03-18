c6d89aedb00d23f71b2e942e53ccc2656b34be33
mendelyv <1064137887@qq.com>
2026-03-18 22:36:40 +0800

    feat:mapping
    - full window
    - open explorer from current file on windows

--- lua/base/mapping.lua
+++ lua/base/mapping.lua
@@ -1,4 +1,5 @@
 local map_util = require("utils.keymap")
+local common = require("utils.common")
 
 local M = {}
 
@@ -14,9 +15,12 @@ map_util.register_all({
     mode = { "n" },
     lhs = "<c-w>a",
     rhs = function()
-      vim.cmd("NvimTreeClose")
+      local tree = require("nvim-tree.api").tree
+      if nil ~= tree and tree.is_visible then tree.close() end
       vim.cmd("wincmd _")
       vim.cmd("wincmd |")
+      vim.cmd("wincmd 5-")
+      vim.cmd("wincmd 20<")
     end,
     options = { silent = true },
     description = "Full screen current buffer",
@@ -30,9 +34,20 @@ map_util.register_all({
   {
     mode = { "n" },
     lhs = "<c-q>",
-    rhs = "<cmd>bdelete<cr>",
+    rhs = function()
+      require("utils.buffer").close_current()
+    end,
     options = { silent = true },
     description = "Close current buffer",
+  },
+  {
+    mode = { "n" },
+    lhs = "<leader>abq",
+    rhs = function()
+      require("utils.buffer").close_current(true)
+    end,
+    options = { silent = true },
+    description = "Force close current buffer",
   },
   {
     mode = { "n" },
@@ -58,5 +73,27 @@ map_util.register_all({
     description = "Close all other buffers",
   },
 })
+
+local function windows_open_current_directory_keymap()
+  if not common.is_windows() then return end
+  map_util.register({
+    mode = { "n" },
+    lhs = "<leader>afs",
+    rhs = function()
+      local dir = vim.fn.expand("%:p:h")
+      if dir == "" then
+        vim.notify("Buffer has no file path", vim.log.levels.WARN)
+        return
+      end
+      dir = vim.fn.system({ "cygpath", "-w", dir }):gsub("\n", "")
+      local cmd = "explorer " .. vim.fn.shellescape(dir)
+      vim.fn.jobstart(cmd, { detach = true })
+    end,
+    options = { silent = true },
+    description = "Open current directory in Windows Explorer",
+  })
+end
+
+windows_open_current_directory_keymap()
 
 return M
